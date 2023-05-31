ac14d7f3ce38daa79d1d033f254385d31ca6a9ce
冬風雪花 <47716286+ToufuSekka@users.noreply.github.com>
2023-05-31 14:59:35 +0900

    실험중1

---
+++ JSPage/EventPages.js
@@ -0,0 +1,19 @@
+let MouseC = document.getElementById('SpecifInfo');
+
+function DescView(M_POS) {
+	MouseC.style.left = M_POS.clientX + "px";
+	MouseC.style.top = M_POS.clientY + "px";
+	MouseC.style.display = "block";
+}
+
+function DescPopping(M_POS, HTMLDataLink) {
+	var FullLink = "DescriptionPage/" + HTMLDataLink + ".html";
+	MouseC.style.left = M_POS.clientX + "px";
+	MouseC.style.top = M_POS.clientY + "px";
+	MouseC.style.display = "block";
+	MouseC.innerHTML = '<object type="text/html" data="' + FullLink + '"></object>';
+}
+
+function DescHide() {
+	MouseC.style.display = "none";
+}
