daec353feb59f179c3e5ca9b718ab8474b14fa7f
冬風雪花 <47716286+ToufuSekka@users.noreply.github.com>
2023-06-06 14:29:38 +0900

    0

--- JSPage/EventPages.js
+++ JSPage/EventPages.js
@@ -8,9 +8,7 @@ function DescView(M_POS) {
 
 function DescPopping(M_POS, HTMLDataLink) {
 	var FullLink = "DescriptionPage/" + HTMLDataLink + ".html";
-	MouseC.style.left = M_POS.clientX + "px";
-	MouseC.style.top = M_POS.clientY + "px";
-	MouseC.style.display = "block";
+	DescView(M_POS);
 	MouseC.innerHTML = '<object type="text/html" data="' + FullLink + '"></object>';
 }
 
